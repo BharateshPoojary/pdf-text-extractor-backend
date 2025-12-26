5e80b00ad2764c9a669f9ecbb37871b6de7469e4
BharateshPoojary <bharateshpoojari@gmail.com>
2025-12-26 07:10:50 +0530

    change multer location

--- src/routes/uploads/index.ts
+++ src/routes/uploads/index.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import {  handleUploadAndDocExtraction } from "../../controllers/uploads/upload.controller";
+import { handleUploadAndDocExtraction } from "../../controllers/uploads/upload.controller";
 import multer from "multer";
 
-const upload = multer({ dest: "pdfs/" });
+const upload = multer({ dest: "/tmp/pdfs" });
 
 const router = Router();
 router.get("/", (_, res) => {
-   return res.json({
+  return res.json({
     message: "I am upload File ",
   });
 });
